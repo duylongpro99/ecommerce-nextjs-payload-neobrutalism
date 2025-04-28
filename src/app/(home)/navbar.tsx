8c33129f4a52b376d5935e696da845fc55ebbdff
longdao0902 <longdaobk17@gmail.com>
2025-04-29 06:53:40 +0700

    feat: add pages

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -15,7 +18,18 @@ interface NavbarItemProps {
 }
 
 const NavbarItem = ({ children, isActive, href }: NavbarItemProps) => {
-  return <Button>{children}</Button>;
+  return (
+    <Button
+      asChild
+      variant={"outline"}
+      className={cn(
+        "bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg",
+        isActive && "bg-black text-white hover:bg-black hover:text-white",
+      )}
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
 };
 
 const navbarItems = [
@@ -47,6 +61,8 @@ const navbarItems = [
 ];
 
 export const Navbar = () => {
+  const pathName = usePathname();
+
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
@@ -56,7 +72,11 @@ export const Navbar = () => {
       </Link>
       <div className="items-center gap-4 hidden lg:flex">
         {navbarItems.map((item) => (
-          <NavbarItem key={item.href} {...item}>
+          <NavbarItem
+            key={item.href}
+            href={item.href}
+            isActive={pathName === item.href}
+          >
             {item.children}
           </NavbarItem>
         ))}
